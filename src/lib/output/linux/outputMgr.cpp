09a9558d0c8305c9bbcbe290aa57e25c403cb760
William Shackleton <w.shackleton@gmail.com>
2012-06-01 21:20:12 +0100

    Fixed direction of axes on Linux.

--- src/lib/output/linux/outputMgr.cpp
+++ src/lib/output/linux/outputMgr.cpp
@@ -90,16 +90,16 @@ void OutputManager::SendJSData(const DPJSData& data, bool firstIteration) {
 }
 
 void OutputManager::SendMouseData(const DPMouseData& data, bool firstIteration) {
-	dpinput_send2Pos(dpinput, data.x / 400, data.y / 400); // TODO: Customise?
-	dpinput_sendPos(dpinput, REL_WHEEL, firstIteration ? (-data.incrementalScrollDelta / 120) : 0);
+	dpinput_send2Pos(dpinput, data.x / 400, -data.y / 400); // TODO: Customise?
+	dpinput_sendPos(dpinput, REL_WHEEL, firstIteration ? (data.incrementalScrollDelta / 120) : 0);
 	dpinput_sendButton(dpinput, BTN_LEFT, data.bLeft);
 	dpinput_sendButton(dpinput, BTN_MIDDLE, data.bMiddle);
 	dpinput_sendButton(dpinput, BTN_RIGHT, data.bRight);
 }
 
 void OutputManager::SendTouchData(const decode::DPTouchData& data, bool firstIteration) {
-	dpinput_send2Pos(dpinput, data.x, data.y);
-	dpinput_sendPos(dpinput, REL_WHEEL, firstIteration ? (-data.incrementalScrollDelta / 120) : 0);
+	dpinput_send2Pos(dpinput, data.x, -data.y);
+	dpinput_sendPos(dpinput, REL_WHEEL, firstIteration ? (data.incrementalScrollDelta / 120) : 0);
 	dpinput_sendButton(dpinput, BTN_LEFT, data.bLeft);
 	dpinput_sendButton(dpinput, BTN_MIDDLE, data.bMiddle);
 	dpinput_sendButton(dpinput, BTN_RIGHT, data.bRight);
